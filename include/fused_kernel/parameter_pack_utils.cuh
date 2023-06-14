d1501bdb76444bea7bfc7680272b7c59082fd0a7
morousg <morousg@gmail.com>
2023-06-14 21:24:15 +0200

    Copyright clarification

--- include/fused_kernel/parameter_pack_utils.cuh
+++ include/fused_kernel/parameter_pack_utils.cuh
@@ -1,4 +1,5 @@
 /* Copyright 2023 Mediaproduccion S.L.U. (Oscar Amoros Huguet)
+   Copyright 2023 Oscar Amoros Huguet
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
