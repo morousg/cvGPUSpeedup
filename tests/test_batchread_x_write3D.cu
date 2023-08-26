79b219d4443e51fe9f2899c3a8cc1f53d73b8eb3
morousg <morousg@gmail.com>
2023-08-27 01:55:23 +0200

    Adding benchmarks to generate csv files with speedup of average times and variance improvement ratio

--- tests/test_batchread_x_write3D.cu
+++ tests/test_batchread_x_write3D.cu
@@ -41,13 +41,13 @@ bool test_batchread_x_write3D(int NUM_ELEMS_X, int NUM_ELEMS_Y, cv::cuda::Stream
             {{1u, 2u, 3u, 4u}, {alpha, alpha, alpha, alpha}, {1.f, 4.f, 3.2f, 0.5f}, {3.2f, 0.6f, 11.8f, 33.f}}
         };
 
-        cv::Scalar val_init = params.at(CV_MAT_CN(CV_TYPE_O)-1).init;
-        cv::Scalar val_alpha = params.at(CV_MAT_CN(CV_TYPE_O)-1).alpha;
-        cv::Scalar val_sub = params.at(CV_MAT_CN(CV_TYPE_O)-1).val_sub;
-        cv::Scalar val_div = params.at(CV_MAT_CN(CV_TYPE_O)-1).val_div;
+        cv::Scalar val_init = params.at(CV_MAT_CN(CV_TYPE_O) - 1).init;
+        cv::Scalar val_alpha = params.at(CV_MAT_CN(CV_TYPE_O) - 1).alpha;
+        cv::Scalar val_sub = params.at(CV_MAT_CN(CV_TYPE_O) - 1).val_sub;
+        cv::Scalar val_div = params.at(CV_MAT_CN(CV_TYPE_O) - 1).val_div;
 
         try {
-            const cv::Size cropSize(60, 120); 
+            const cv::Size cropSize(60, 120);
             cv::cuda::GpuMat d_input(NUM_ELEMS_Y, NUM_ELEMS_X, CV_TYPE_I, val_init);
             std::array<cv::cuda::GpuMat, BATCH> d_output_cv;
             std::array<cv::Mat, BATCH> h_cvResults;
@@ -56,49 +56,53 @@ bool test_batchread_x_write3D(int NUM_ELEMS_X, int NUM_ELEMS_Y, cv::cuda::Stream
             cv::cuda::GpuMat d_temp(cropSize, CV_TYPE_O);
             cv::cuda::GpuMat d_temp2(cropSize, CV_TYPE_O);
 
-            cv::cuda::GpuMat d_tensor_output(BATCH, 
-                                             cropSize.width * cropSize.height,
-                                             CV_TYPE_O);
+            cv::cuda::GpuMat d_tensor_output(BATCH,
+                cropSize.width * cropSize.height,
+                CV_TYPE_O);
             d_tensor_output.step = cropSize.width * cropSize.height * sizeof(CUDA_T(CV_TYPE_O));
 
             cv::Mat diff(cropSize, CV_TYPE_O);
             cv::Mat h_tensor_output(BATCH, cropSize.width * cropSize.height, CV_TYPE_I);
 
             std::array<cv::cuda::GpuMat, BATCH> crops;
-            for (int crop_i=0; crop_i<BATCH; crop_i++) {
+            for (int crop_i = 0; crop_i < BATCH; crop_i++) {
                 crops[crop_i] = cv::cuda::GpuMat(cropSize, CV_TYPE_I, val_init);
                 d_output_cv[crop_i].create(cropSize, CV_TYPE_O);
                 h_cvResults[crop_i].create(cropSize, CV_TYPE_O);
             }
 
+            START_OCV_BENCHMARK
             // OpenCV version
-            for (int crop_i=0; crop_i<BATCH; crop_i++) {
+            for (int crop_i = 0; crop_i < BATCH; crop_i++) {
                 crops[crop_i].convertTo(d_temp, CV_TYPE_O, alpha, cv_stream);
                 cv::cuda::subtract(d_temp, val_sub, d_temp2, cv::noArray(), -1, cv_stream);
                 cv::cuda::divide(d_temp2, val_div, d_output_cv[crop_i], 1.0, -1, cv_stream);
             }
 
+            STOP_OCV_START_CVGS_BENCHMARK
             // cvGPUSpeedup
             // Assuming we use all the batch
             // On Linux it is necessary to pass the BATCH as a template parameter
             // On Windows (VS2022 Community) it is not needed, it is deduced from crops 
             cvGS::executeOperations<BATCH>(crops, BATCH, cv_stream,
-                                                cvGS::convertTo<CV_TYPE_I, CV_TYPE_O>(),
-                                                cvGS::multiply<CV_TYPE_O>(val_alpha),
-                                                cvGS::subtract<CV_TYPE_O>(val_sub),
-                                                cvGS::divide<CV_TYPE_O>(val_div),
-                                                cvGS::write<CV_TYPE_O>(d_tensor_output, cropSize));
+                cvGS::convertTo<CV_TYPE_I, CV_TYPE_O>(),
+                cvGS::multiply<CV_TYPE_O>(val_alpha),
+                cvGS::subtract<CV_TYPE_O>(val_sub),
+                cvGS::divide<CV_TYPE_O>(val_div),
+                cvGS::write<CV_TYPE_O>(d_tensor_output, cropSize));
 
-            d_tensor_output.download(h_tensor_output, cv_stream);
+            STOP_CVGS_BENCHMARK
+
+                d_tensor_output.download(h_tensor_output, cv_stream);
 
             // Verify results
-            for (int crop_i=0; crop_i<BATCH; crop_i++) {
+            for (int crop_i = 0; crop_i < BATCH; crop_i++) {
                 d_output_cv[crop_i].download(h_cvResults[crop_i], cv_stream);
             }
 
             cv_stream.waitForCompletion();
 
-            for (int crop_i=0; crop_i<BATCH; crop_i++) {
+            for (int crop_i = 0; crop_i < BATCH; crop_i++) {
                 cv::Mat cvRes = h_cvResults[crop_i];
                 cv::Mat cvGSRes = cv::Mat(cropSize.height, cropSize.width, CV_TYPE_O, h_tensor_output.row(crop_i).data);
                 bool passedThisTime = compareAndCheck<CV_TYPE_O>(cropSize.width, cropSize.height, cvRes, cvGSRes);
@@ -115,7 +119,7 @@ bool test_batchread_x_write3D(int NUM_ELEMS_X, int NUM_ELEMS_Y, cv::cuda::Stream
             error_s << e.what();
             passed = false;
             exception = true;
-        } 
+        }
 
         if (!passed) {
             if (!exception) {
@@ -144,9 +148,17 @@ int main() {
     std::unordered_map<std::string, bool> results;
     results["test_batchread_x_write3D"] = true;
 
-    #define LAUNCH_TESTS(CV_INPUT, CV_OUTPUT) \
-    results["test_batchread_x_write3D"] &= test_batchread_x_write3D<CV_INPUT, CV_OUTPUT, 50>(NUM_ELEMS_X, NUM_ELEMS_Y, cv_stream, true);
+#define LAUNCH_TESTS(CV_INPUT, CV_OUTPUT) \
+    results["test_batchread_x_write3D"] &= test_batchread_x_write3D<CV_INPUT, CV_OUTPUT, 1>(NUM_ELEMS_X, NUM_ELEMS_Y, cv_stream, true); \
+    results["test_batchread_x_write3D"] &= test_batchread_x_write3D<CV_INPUT, CV_OUTPUT, 10>(NUM_ELEMS_X, NUM_ELEMS_Y, cv_stream, true); \
+    results["test_batchread_x_write3D"] &= test_batchread_x_write3D<CV_INPUT, CV_OUTPUT, 30>(NUM_ELEMS_X, NUM_ELEMS_Y, cv_stream, true); \
+    results["test_batchread_x_write3D"] &= test_batchread_x_write3D<CV_INPUT, CV_OUTPUT, 50>(NUM_ELEMS_X, NUM_ELEMS_Y, cv_stream, true); \
+    results["test_batchread_x_write3D"] &= test_batchread_x_write3D<CV_INPUT, CV_OUTPUT, 100>(NUM_ELEMS_X, NUM_ELEMS_Y, cv_stream, true);
 
+#ifdef ENABLE_BENCHMARK
+    // Warming up for the benchmarks
+    results["test_batchread_x_write3D"] &= test_batchread_x_write3D<CV_8UC1, CV_32FC1, 5>(NUM_ELEMS_X, NUM_ELEMS_Y, cv_stream, true);
+#endif
     LAUNCH_TESTS(CV_8UC1, CV_32FC1)
     LAUNCH_TESTS(CV_8SC1, CV_32FC1)
     LAUNCH_TESTS(CV_16UC1, CV_32FC1)
@@ -172,6 +184,8 @@ int main() {
     LAUNCH_TESTS(CV_32FC3, CV_64FC3)
     LAUNCH_TESTS(CV_32FC4, CV_64FC4)
 
+    CLOSE_BENCHMARK
+
     for (const auto& [key, passed] : results) {
         if (passed) {
             std::cout << key << " passed!!" << std::endl;
@@ -180,7 +194,7 @@ int main() {
         }
     }
 
-    #undef LAUNCH_TESTS
+#undef LAUNCH_TESTS
 
     return 0;
 }
--- tests/testsCommon.cuh
+++ tests/testsCommon.cuh
@@ -16,6 +16,9 @@
 #include <cv2cuda_types.cuh>
 
 #include <sstream>
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
 
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
@@ -31,9 +34,9 @@ bool checkResults(int NUM_ELEMS_X, int NUM_ELEMS_Y, cv::Mat& h_comparison1C) {
     cv::compare(h_comparison1C, maxError, h_comparison, cv::CMP_GT);
 
 #ifdef CVGS_DEBUG
-    for (int y=0; y<h_comparison1C.rows; y++) {
-        for (int x=0; x<h_comparison1C.cols; x++) {
-            CUDA_T(T) value = h_comparison1C.at<CUDA_T(T)>(y,x);
+    for (int y = 0; y < h_comparison1C.rows; y++) {
+        for (int x = 0; x < h_comparison1C.cols; x++) {
+            CUDA_T(T) value = h_comparison1C.at<CUDA_T(T)>(y, x);
             if (value > 0.00001) {
                 std::cout << "(" << x << "," << y << ")= " << value << ";" << std::endl;
             }
@@ -41,7 +44,7 @@ bool checkResults(int NUM_ELEMS_X, int NUM_ELEMS_Y, cv::Mat& h_comparison1C) {
         std::cout << std::endl;
     }
 #endif
-    
+
     int errors = cv::countNonZero(h_comparison);
     return errors == 0;
 }
@@ -53,8 +56,109 @@ bool compareAndCheck(int NUM_ELEMS_X, int NUM_ELEMS_Y, cv::Mat& cvVersion, cv::M
     std::vector<cv::Mat> h_comparison1C(CV_MAT_CN(T));
     cv::split(diff, h_comparison1C);
 
-    for (int i=0; i<CV_MAT_CN(T); i++) {
+    for (int i = 0; i < CV_MAT_CN(T); i++) {
         passed &= checkResults<CV_MAT_DEPTH(T)>(NUM_ELEMS_X, NUM_ELEMS_Y, h_comparison1C.at(i));
     }
     return passed;
 }
+
+#ifdef ENABLE_BENCHMARK
+std::unordered_map<std::string, std::stringstream> benchmarkResultsText;
+std::unordered_map < std::string, std::ofstream> currentFile;
+// Select the path where to write the benchmark files
+const std::string path{ "" };
+
+constexpr int ITERS = 1000;
+
+struct BenchmarkResultsNumbers {
+    float OCVelapsedTimeMax;
+    float OCVelapsedTimeAcum;
+    float cvGSelapsedTimeMax;
+    float cvGSelapsedTimeAcum;
+};
+
+template <int ITERATIONS>
+float computeVariance(const float& mean, const std::array<float, ITERATIONS>& times) {
+    float sumOfDiff = 0.f;
+    for (int i = 0; i < ITERATIONS; i++) {
+        const float diff = times[i] - mean;
+        sumOfDiff += (diff * diff);
+    }
+    return sumOfDiff / (ITERATIONS - 1);
+}
+
+template <int CV_INPUT_TYPE, int CV_OUTPUT_TYPE, int BATCH, int ITERATIONS>
+void processExecution(const BenchmarkResultsNumbers& resF, const std::string& functionName,
+    const std::array<float, ITERS>& OCVelapsedTime, const std::array<float, ITERS>& cvGSelapsedTime) {
+    if constexpr (BATCH == 1) {
+        const std::string fileName = functionName + std::string(".csv");
+        if (currentFile.find(fileName) == currentFile.end()) {
+            currentFile[fileName].open(path + fileName);
+            currentFile[fileName] << "Number of images, Avg1, Var1, Avg10, Var10, Avg30, Var30, Avg50, Var50, Avg100, Var100\n";
+        }
+        benchmarkResultsText.clear();
+        benchmarkResultsText["SpeedupLine"] << "Speedup " << cvTypeToString<CV_INPUT_TYPE>() << "_" << cvTypeToString<CV_OUTPUT_TYPE>();
+    }
+    const float ocvMean = resF.OCVelapsedTimeAcum / ITERATIONS;
+    const float cvgsMean = resF.cvGSelapsedTimeAcum / ITERATIONS;
+    const float ocvVariance = computeVariance(ocvMean, OCVelapsedTime);
+    const float cvgsVariance = computeVariance(cvgsMean, cvGSelapsedTime);
+
+    benchmarkResultsText["SpeedupLine"] << ", " << (ocvMean / cvgsMean);
+    benchmarkResultsText["SpeedupLine"] << ", " << (ocvVariance / cvgsVariance);
+    if constexpr (BATCH == 100) {
+        const std::string fileName = functionName + std::string(".csv");
+        currentFile[fileName] << benchmarkResultsText["SpeedupLine"].str() << std::endl;
+    }
+}
+
+#endif
+
+#ifdef ENABLE_BENCHMARK
+#define START_OCV_BENCHMARK \
+cudaEvent_t start, stop; \
+BenchmarkResultsNumbers resF{0.f, 0.f, 0.f, 0.f}; \
+cudaStream_t stream = cv::cuda::StreamAccessor::getStream(cv_stream); \
+gpuErrchk(cudaEventCreate(&start)); \
+gpuErrchk(cudaEventCreate(&stop)); \
+std::array<float, ITERS> OCVelapsedTime; \
+std::array<float, ITERS> cvGSelapsedTime; \
+for (int i = 0; i < ITERS; i++) { \
+    gpuErrchk(cudaEventRecord(start, stream));
+#else
+#define START_OCV_BENCHMARK
+#endif
+
+#ifdef ENABLE_BENCHMARK
+#define STOP_OCV_START_CVGS_BENCHMARK \
+gpuErrchk(cudaEventRecord(stop, stream)); \
+gpuErrchk(cudaEventSynchronize(stop)); \
+gpuErrchk(cudaEventElapsedTime(&OCVelapsedTime[i], start, stop)); \
+resF.OCVelapsedTimeMax = resF.OCVelapsedTimeMax < OCVelapsedTime[i] ? OCVelapsedTime[i] : resF.OCVelapsedTimeMax; \
+resF.OCVelapsedTimeAcum += OCVelapsedTime[i]; \
+gpuErrchk(cudaEventRecord(start, stream));
+#else
+#define STOP_OCV_START_CVGS_BENCHMARK
+#endif
+
+#ifdef ENABLE_BENCHMARK
+#define STOP_CVGS_BENCHMARK \
+gpuErrchk(cudaEventRecord(stop, stream)); \
+gpuErrchk(cudaEventSynchronize(stop)); \
+gpuErrchk(cudaEventElapsedTime(&cvGSelapsedTime[i], start, stop)); \
+resF.cvGSelapsedTimeMax = resF.cvGSelapsedTimeMax < cvGSelapsedTime[i] ? cvGSelapsedTime[i] : resF.cvGSelapsedTimeMax; \
+resF.cvGSelapsedTimeAcum += cvGSelapsedTime[i]; \
+} \
+processExecution<CV_TYPE_I, CV_TYPE_O, BATCH, ITERS>(resF, __func__, OCVelapsedTime, cvGSelapsedTime);
+#else
+#define STOP_CVGS_BENCHMARK
+#endif
+
+#ifdef ENABLE_BENCHMARK
+#define CLOSE_BENCHMARK \
+for (auto&& [_, file] : currentFile) { \
+    file.close(); \
+}
+#else
+#define CLOSE_BENCHMARK
+#endif
