d4c834bbc81d5d65d83218a7f7276229ffe95fc2
morousg <morousg@gmail.com>
2023-12-14 22:35:13 +0100

    Refactor: replaced thrust::tuple with our own Tuple implementation

--- include/fused_kernel/core/utils/parameter_pack_utils.cuh
+++ include/fused_kernel/core/utils/parameter_pack_utils.cuh
@@ -15,32 +15,31 @@
 
 #pragma once
 
-#include <thrust/tuple.h>
-#include <thrust/functional.h>
 #include <fused_kernel/core/utils/cuda_utils.cuh>
+#include <fused_kernel/core/utils/tuple.cuh>
 
 namespace fk { // namespace fused kernel
 
     template <typename F, typename Tuple, size_t... I>
-    FK_HOST_DEVICE_CNST auto apply_impl(F&& f, Tuple&& t, std::index_sequence<I...>)
-        -> decltype(std::forward<F>(f)(thrust::get<I>(std::forward<Tuple>(t))...))
+    FK_HOST_DEVICE_CNST auto apply_impl(F&& f, Tuple& t, std::index_sequence<I...>)
+        -> decltype(std::forward<F>(f)(get_v<I>(std::forward<Tuple>(t))...))
     {
-        return std::forward<F>(f)(thrust::get<I>(std::forward<Tuple>(t))...);
+        return std::forward<F>(f)(get_v<I>(std::forward<Tuple>(t))...);
     }
 
     template <typename F, typename Tuple>
-    FK_HOST_DEVICE_CNST auto apply(F&& f, Tuple&& t)
+    FK_HOST_DEVICE_CNST auto apply(F&& f, Tuple& t)
         -> decltype(apply_impl(std::forward<F>(f), std::forward<Tuple>(t),
-            std::make_index_sequence<thrust::tuple_size<typename std::decay<Tuple>::type>::value>()))
+            std::make_index_sequence<Tuple::size>()))
     {
         return apply_impl(std::forward<F>(f), std::forward<Tuple>(t),
-            std::make_index_sequence<thrust::tuple_size<typename std::decay<Tuple>::type>::value>());
+            std::make_index_sequence<Tuple::size>());
     }
 
     // Struct to hold a parameter pack, and be able to pass it arround
     template <typename... DeviceFunctionTypes>
     struct DeviceFunctionSequence {
-        thrust::tuple<const DeviceFunctionTypes...> deviceFunctions;
+        Tuple<const DeviceFunctionTypes...> deviceFunctions;
     };
 
     // Function that fills the OperationSequence struct, from a parameter pack
@@ -50,7 +49,7 @@ namespace fk { // namespace fused kernel
     }
 
     template <typename... DeviceFunctionTypes>
-    FK_HOST_DEVICE_CNST auto buildOperationSequence_tup(const thrust::tuple<DeviceFunctionTypes...>& deviceFunctionInstances) {
+    FK_HOST_DEVICE_CNST auto buildOperationSequence_tup(const Tuple<DeviceFunctionTypes...>& deviceFunctionInstances) {
         return fk::apply([](const auto&... args) { 
                             return buildOperationSequence(args...); 
                          }, deviceFunctionInstances);
@@ -72,44 +71,14 @@ namespace fk { // namespace fused kernel
         return t;
     }
 
-    // Util to concatenate thrust::tuple
-    template <typename Tuple1, typename Tuple2, int... I1, int... I2>
-    FK_HOST_DEVICE_CNST
-    auto tuple_cat_impl(const Tuple1& t1, std::integer_sequence<int, I1...>, const Tuple2& t2, std::integer_sequence<int, I2...>) {
-        static_assert(thrust::tuple_size<Tuple1>::value + thrust::tuple_size<Tuple2>::value <= 10,
-                      "thrust::tuple max size is 10, you are trying to create a bigger tuple");
-        return thrust::make_tuple(thrust::get<I1>(t1)..., thrust::get<I2>(t2)...);
-    }
-
-    template <typename Tuple1, typename Tuple2>
-    FK_HOST_DEVICE_CNST
-    auto tuple_cat(const Tuple1& t1, const Tuple2& t2) {
-        return tuple_cat_impl(t1, std::make_integer_sequence<int, thrust::tuple_size<Tuple1>::value>(),
-            t2, std::make_integer_sequence<int, thrust::tuple_size<Tuple2>::value>());
-    }
-
     // Util to insert an element before the last element of a tuple
     template <typename T, typename Tuple>
     FK_HOST_DEVICE_CNST auto insert_before_last_tup(const T& t, const Tuple& args) {
-        if constexpr (thrust::tuple_size<Tuple>::value == 1) {
-            return fk::tuple_cat(thrust::make_tuple(t), args);
-        } else {
-            const auto [head, tail] = args;
-            return fk::tuple_cat(thrust::make_tuple(head), insert_before_last_tup(t, tail));
-        }
+        return tuple_insert<Tuple::size - 1>(t, args);
     }
 
     template<typename T, typename... Args>
     FK_HOST_DEVICE_CNST auto insert_before_last(const T& t, const Args&... args) {
-        return insert_before_last_tup(t, thrust::tuple<const Args...>{args...});
+        return tuple_insert<sizeof...(Args) - 1>(t, Tuple<Args...>{args...});
     }
-
-    template <typename T>
-    struct is_thrust_tuple : std::false_type {};
-
-    template <typename... Args>
-    struct is_thrust_tuple<thrust::tuple<Args...>> : std::true_type {};
-
-    template <typename T>
-    constexpr bool is_thrust_tuple_v = is_thrust_tuple<T>::value;
 } // namespace fused kernel
