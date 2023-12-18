9c4bd4c39df70e8d3b6f78ddb52a8ae69bab0f07
morousg <morousg@gmail.com>
2023-12-18 13:07:01 +0100

    Fix: fixed benchmarks to work with the latest changes

--- tests/benchmark/benchmark_vertical_fusionMAD_loop.cu
+++ tests/benchmark/benchmark_vertical_fusionMAD_loop.cu
@@ -39,7 +39,7 @@ struct VerticalFusionMAD {
                                const cv::Size& cropSize) {
         using InputType = CUDA_T(CV_TYPE_I);
         using OutputType = CUDA_T(CV_TYPE_O);
-        using Loop = fk::Binary<fk::StaticLoop<fk::StaticLoop<fk::ComposedOperationSequence<fk::Mul<OutputType>, fk::Sum<OutputType>>, INCREMENT / 2>, NumOps / INCREMENT>>;
+        using Loop = fk::Binary<fk::StaticLoop<fk::StaticLoop<fk::OperationTupleOperation<fk::Mul<OutputType>, fk::Sum<OutputType>>, INCREMENT / 2>, NumOps / INCREMENT>>;
         cvGS::executeOperations(crops, BATCH, cv_stream,
             cvGS::convertTo<CV_TYPE_I, CV_TYPE_O>((float)alpha),
             Loop{ {{{cvGS::cvScalar2CUDAV<CV_TYPE_O>::get(val_mul)},{cvGS::cvScalar2CUDAV<CV_TYPE_O>::get(val_add)}}} },
