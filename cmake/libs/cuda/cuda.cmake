f76e4dff0dbff1214d1440df239119ab35c778bc
Albert Andaluz <aandaluz@mediapro.tv>
2024-02-10 20:45:11 +0100

    Issue 91: parse cuda version from version.json provided by nvidia with cuda sdk toolkit
    use this version for deploy.cmake
    deploy cudblas and cublaslt with opencv
    also deploy npp dll  for opencv

--- cmake/libs/cuda/cuda.cmake
+++ cmake/libs/cuda/cuda.cmake
@@ -4,16 +4,34 @@ include(cmake/libs/cuda/target_generation.cmake)
 include(cmake/libs/cuda/deploy.cmake)
 
 
-find_package(CUDAToolkit ${CUDA_NVCC_VERSION_FROM_VERSION_FILE} REQUIRED)
+find_package(CUDAToolkit REQUIRED)
 # extra cuda_libraries only detected after project() this is needed for compatibility with old local builds that only
 # have cuda in normal location instead of custom location
-find_package(CUDAToolkit ${CUDA_NVCC_VERSION_FROM_VERSION_FILE} REQUIRED)
+ 
 # some external lbis(opencv) use findCuda, so we set this variable for compatibility
 set(CUDA_TOOLKIT_ROOT_DIR_ORIG ${CUDAToolkit_LIBRARY_ROOT})
 # file(TO_CMAKE_PATH $ENV{APIS_PATH_VS2017} APIS_PATH)
 string(REPLACE "\\" "/" CUDA_TOOLKIT_ROOT_DIR_ORIG ${CUDA_TOOLKIT_ROOT_DIR_ORIG})
 set(CUDA_TOOLKIT_ROOT_DIR ${CUDA_TOOLKIT_ROOT_DIR_ORIG})
+ 
 
+function(get_cuda_component_version COMPONENT COMPONENT_VERSION)
+    file(READ ${CUDAToolkit_LIBRARY_ROOT}/version.json CUDA_VERSION_FILE_JSON_STRING)
+    string(JSON COMPONENT_JSON_STRING GET ${CUDA_VERSION_FILE_JSON_STRING} ${IDX} ${COMPONENT})
+    string(JSON COMPONENT_JSON_STRING_1 GET ${COMPONENT_JSON_STRING} ${IDX} version)
+    set(${COMPONENT_VERSION} ${COMPONENT_JSON_STRING_1} PARENT_SCOPE)
+endfunction()
+
+# Get the name from the current JSON element.
+get_cuda_component_version("cuda" CUDA_VERSION_FROM_VERSION_FILE)
+# findcudatookit requires nvcc version instead of cuda sdk version
+get_cuda_component_version("cuda_nvcc" CUDA_NVCC_VERSION_FROM_VERSION_FILE)
+ 
+ # split cuda version string
+ string(REGEX REPLACE "([0-9]+).[0-9]+.[0-9]+" "\\1" CUDA_VERSION_MAJOR ${CUDA_VERSION_FROM_VERSION_FILE})
+ string(REGEX REPLACE "[0-9]+.([0-9]+).[0-9]+" "\\1" CUDA_VERSION_MINOR ${CUDA_VERSION_FROM_VERSION_FILE})
+ string(REGEX REPLACE "[0-9]+.[0-9]+.([0-9]+)" "\\1" CUDA_VERSION_REVISION ${CUDA_VERSION_FROM_VERSION_FILE})
+ 
 
 function(add_cuda_to_target TARGET_NAME COMPONENTS)
     set_default_cuda_target_properties(${TARGET_NAME})
--- cmake/libs/opencv.cmake
+++ cmake/libs/opencv.cmake
@@ -15,11 +15,11 @@ function(add_opencv_to_target TARGET_NAME COMPONENTS)
         deploy_exported_target_dependencies(${TARGET_NAME} ${EXPORTED_TARGETS})
     endif()
     # cuda libraries  implicit dependencies for cuda modules
-
+    set(NPP_COMPONENTS nppc nppial nppicc nppig nppidei nppitc nppist nppc nppif nppim npps)
     if(NOT UNIX)
         deploy_cuda_dependencies(${TARGET_NAME} "${NPP_COMPONENTS}")
         # cudaarithm
-        deploy_cuda_dependencies(${TARGET_NAME} cufft)
+        deploy_cuda_dependencies(${TARGET_NAME} "cufft;cublas;cublasLt")
 
     endif()
 
