9e66e53200405d254cf9eb3a704bd43ade8d1501
morousg <morousg@gmail.com>
2024-03-23 20:00:46 +0100

    Feature: allow to compile the fused_kernel_library tests, without OpenCV

--- cmake/libs/opencv.cmake
+++ cmake/libs/opencv.cmake
@@ -1,16 +1,12 @@
-#note: you must build opencv with cuda support (vanilla builds don't provide cuda modules)
-#remember to set OpenCV_DIR enviroment varialbe to find opencv or use pkgconfig
-find_package(OpenCV ${OPENCV_VERSION} REQUIRED)
+set ($ENV{OPENCV_DIR} "")
+find_package(OpenCV ${OPENCV_VERSION})
 
 function(add_opencv_to_target TARGET_NAME COMPONENTS)
     set(EXPORTED_TARGETS ${COMPONENTS})
-    #if(WIN32)
-     #   list(TRANSFORM EXPORTED_TARGETS PREPEND "opencv::")
-    #elseif(UNIX)
-        list(TRANSFORM EXPORTED_TARGETS PREPEND "opencv_")
-    #endif()
-    # https://stackoverflow.com/questions/55640570/using-opencvs-cmake-module-targets-instead-of-including-all-libraries-directly
-    target_link_libraries(${TARGET_NAME} PRIVATE ${EXPORTED_TARGETS})
+	
+    list(TRANSFORM EXPORTED_TARGETS PREPEND "opencv_")
+    
+	target_link_libraries(${TARGET_NAME} PRIVATE ${EXPORTED_TARGETS})
     if(WIN32)
         deploy_exported_target_dependencies(${TARGET_NAME} ${EXPORTED_TARGETS})
     endif()
