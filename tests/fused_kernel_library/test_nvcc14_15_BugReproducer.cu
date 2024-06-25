2d114647264e5bb3e257366f5bbd35e77814ba2f
Oscar Amoros Huguet <morousg@gmail.com>
2024-06-26 00:31:03 +0200

    Fixed copyright message

--- tests/fused_kernel_library/test_nvcc14_15_BugReproducer.cu
+++ tests/fused_kernel_library/test_nvcc14_15_BugReproducer.cu
@@ -1,4 +1,4 @@
-/* Copyright 2023 Mediaproduccion S.L.U. (Oscar Amoros Huguet)
+/* Copyright 2024 Oscar Amoros Huguet
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
