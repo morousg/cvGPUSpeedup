3ff503583d513b2cdb018e637379021e0659cc38
Albert Andaluz <albertandaluz@gmail.com>
2024-08-02 20:02:55 +0200

    added more npp calls

--- benchmarks/npp/benchmark_npp_batchresize_x_split3D.cu
+++ benchmarks/npp/benchmark_npp_batchresize_x_split3D.cu
@@ -18,18 +18,18 @@
 #include <npp.h>
 #include <nppi_geometry_transforms.h>
 #include <cvGPUSpeedup.cuh>
- 
+
 #include "tests/main.h"
 
-constexpr char VARIABLE_DIMENSION[]{ "Batch size" };
+constexpr char VARIABLE_DIMENSION[]{"Batch size"};
 constexpr size_t NUM_EXPERIMENTS = 9;
 constexpr size_t FIRST_VALUE = 10;
 constexpr size_t INCREMENT = 10;
 constexpr std::array<size_t, NUM_EXPERIMENTS> batchValues = arrayIndexSecuence<FIRST_VALUE, INCREMENT, NUM_EXPERIMENTS>;
 constexpr inline void nppAssert(NppStatus code,
-	const char* file,
-	int line,
-	bool abort = true)
+								const char *file,
+								int line,
+								bool abort = true)
 {
 	if (code != NPP_SUCCESS)
 	{
@@ -44,10 +44,9 @@ constexpr inline void nppAssert(NppStatus code,
 		nppAssert((ans), __FILE__, __LINE__, true); \
 	}
 
+NppStreamContext initNppStreamContext(const cudaStream_t &stream);
 
-NppStreamContext initNppStreamContext(const cudaStream_t& stream);
-
-NppStreamContext initNppStreamContext(const cudaStream_t& stream)
+NppStreamContext initNppStreamContext(const cudaStream_t &stream)
 {
 	int device = 0;
 	int ccmajor = 0;
@@ -56,193 +55,164 @@ NppStreamContext initNppStreamContext(const cudaStream_t& stream)
 
 	cudaDeviceProp prop;
 	gpuErrchk(cudaGetDevice(&device))
-		gpuErrchk(cudaGetDeviceProperties(&prop, device));
+	gpuErrchk(cudaGetDeviceProperties(&prop, device));
 	gpuErrchk(cudaDeviceGetAttribute(&ccmajor, cudaDevAttrComputeCapabilityMinor, device));
 	gpuErrchk(cudaDeviceGetAttribute(&ccminor, cudaDevAttrComputeCapabilityMajor, device));
 	gpuErrchk(cudaStreamGetFlags(stream, &flags));
-	NppStreamContext nppstream = { stream, device, prop.multiProcessorCount, prop.maxThreadsPerMultiProcessor,
-		prop.maxThreadsPerBlock, prop.sharedMemPerBlock, ccmajor, ccminor, flags };
+	NppStreamContext nppstream = {stream, device, prop.multiProcessorCount, prop.maxThreadsPerMultiProcessor,
+								  prop.maxThreadsPerBlock, prop.sharedMemPerBlock, ccmajor, ccminor, flags};
 	return nppstream;
 }
 
 template <int BATCH>
-bool test_npp_batchresize_x_split3D(size_t NUM_ELEMS_X, size_t NUM_ELEMS_Y, cudaStream_t& cv_stream, bool enabled) {
+bool test_npp_batchresize_x_split3D(size_t NUM_ELEMS_X, size_t NUM_ELEMS_Y, cudaStream_t &compute_stream, bool enabled)
+{
 	std::stringstream error_s;
 	bool passed = true;
 	bool exception = false;
 
-	if (enabled) {
-		float alpha = 0.3f;
+	if (enabled)
+	{
+		const float alpha = 0.3f;
 
-		uchar3 val_init = { 5u, 5u, 5u };
-		float3 val_alpha = { alpha, alpha, alpha };
-		float3 val_sub = { 1.f, 4.f, 3.2f };
-		float3 val_div = { 1.f, 4.f, 3.2f };
-
-		try {
+		const uchar3 val_init = {5u, 5u, 5u};
+		const float3 val_alpha = {alpha, alpha, alpha};
+		const float3 val_sub = {1.f, 4.f, 3.2f};
+		const float3 val_div = {1.f, 4.f, 3.2f};
+		const uchar CROP_W = 60;
+		const uchar CROP_H = 120;
+		const uchar UP_W = 64;
+		const uchar UP_H = 128;
+		try
+		{
 			fk::Ptr2D<uchar3> d_input((int)NUM_ELEMS_X, (int)NUM_ELEMS_Y);
 
-			
-
-			std::array<cv::Rect2d, BATCH> crops_2d;
+			std::array<fk::Rect2d, BATCH> crops_2d;
 			std::array<NppiImageDescriptor, BATCH> srccropsdesc;
-			for (int crop_i = 0; crop_i < BATCH; crop_i++) {
-			    const Ptr2D<uchar3> d_cropped = d_input.crop2D(fk::Point(crop_i, crop_i), fk::PtrDims<fk::_2D>{60, 120, d_input.ptr().dims.pitch});
-				srccropsdesc[crop_i].pData = (uchar*)d_input(crops_2d[crop_i]).data;
+			for (int crop_i = 0; crop_i < BATCH; crop_i++)
+			{
+				const fk::Ptr2D<uchar3> d_cropped = d_input.crop2D(fk::Point(crop_i, crop_i), fk::PtrDims<fk::_2D>{CROP_W, CROP_H, d_input.ptr().dims.pitch});
+				srccropsdesc[crop_i].pData = (uchar *)d_input(crops_2d[crop_i]).data;
 				srccropsdesc[crop_i].nStep = d_input(crops_2d[crop_i]).step;
-				srccropsdesc[crop_i].oSize = { crop_i + 60, crop_i + 120 };
-
-
+				srccropsdesc[crop_i].oSize = {crop_i + CROP_W, crop_i + CROP_H};
 			}
 
-			cv::Size up(64, 128);
-			cv::cuda::GpuMat d_up(up, CV_TYPE_I);
-			cv::cuda::GpuMat d_temp(up, CV_TYPE_O);
-			cv::cuda::GpuMat d_temp2(up, CV_TYPE_O);
+			const fk::Size up(UP_W, UP_H);
+			fk::Ptr2D<uchar3> d_up(up);
+			fk::Ptr2D<float3> d_temp(up);
+			fk::Ptr2D<float3> d_temp2(up);
 
-			std::array<std::vector<cv::cuda::GpuMat>, BATCH> d_output_cv;
-			std::array<std::vector<cv::cuda::GpuMat>, BATCH> d_output_npp;
-			std::array<std::vector<cv::cuda::GpuMat>, BATCH> d_output_cvGS;
-			std::array<std::vector<cv::Mat>, BATCH> h_cvResults;
-			std::array<std::vector<cv::Mat>, BATCH> h_cvGSResults;
+			std::array<std::vector<fk::Ptr2D<float3>>, BATCH> d_output_crop_npp;
+			std::array<std::vector<fk::Ptr2D<float3>>, BATCH> d_output_cvGS;
 
-			cv::cuda::GpuMat d_tensor_output(BATCH, up.width * up.height * CV_MAT_CN(CV_TYPE_O), CV_MAT_DEPTH(CV_TYPE_O));
-			d_tensor_output.step = up.width * up.height * CV_MAT_CN(CV_TYPE_O) * sizeof(BASE_CUDA_T(CV_TYPE_O));
+			fk::Ptr2D<float3> d_tensor_output(BATCH, up.width * up.height * float3, 3);
+			d_tensor_output.step = up.width * up.height * sizeof(float);
+            std::array<fk::Ptr2D<float3>, BATCH> crops;
 
-			cv::Mat diff(up, CV_MAT_DEPTH(CV_TYPE_O));
-			cv::Mat h_tensor_output(BATCH, up.width * up.height * CV_MAT_CN(CV_TYPE_O), CV_MAT_DEPTH(CV_TYPE_O));
-
-			std::array<cv::cuda::GpuMat, BATCH> crops;
-			
 			std::array<NppiImageDescriptor, BATCH> dstcropsdesc;
-			for (int crop_i = 0; crop_i < BATCH; crop_i++) {
+			for (int crop_i = 0; crop_i < BATCH; crop_i++)
+			{
 				crops[crop_i] = d_input(crops_2d[crop_i]);
-				dstcropsdesc[crop_i].pData = (uchar*)d_output_npp[crop_i].data();
-				
-				dstcropsdesc[crop_i].nStep = d_output_npp[crop_i].step;
-				dstcropsdesc[crop_i].oSize = { up.width,up.height };
-
-				for (int i = 0; i < CV_MAT_CN(CV_TYPE_I); i++) {
-
-					d_output_cv.at(crop_i).emplace_back(up, CV_MAT_DEPTH(CV_TYPE_O));
-					h_cvResults.at(crop_i).emplace_back(up, CV_MAT_DEPTH(CV_TYPE_O));
-				}
+				dstcropsdesc[crop_i].pData = (uchar *)d_output_crop_npp[crop_i].data();
+				dstcropsdesc[crop_i].nStep = d_output_crop_npp[crop_i].step;
+				dstcropsdesc[crop_i].oSize = {up.width, up.height};
 			}
 
-			constexpr bool correctDept = CV_MAT_DEPTH(CV_TYPE_O) == CV_32F;
+			// NPP version
+			NppStreamContext nppcontext = initNppStreamContext(static_cast<cudaStream_t>(compute_stream.cudaPtr()));
 
-			
-				// NPP version
-
-				//cudaStream_t stream;
-			//gpuErrchk(cudaStreamCreate(&stream));
-
-				NppStreamContext nppcontext = initNppStreamContext(static_cast<cudaStream_t>(cv_stream.cudaPtr()));
-
-			if constexpr (CV_MAT_CN(CV_TYPE_I) == 3 && correctDept) {
-				//(for i=0;i<BATCH;++i)
-				//nppiConvert_8u32f_AC3
-				// 
-				//nppiResizeBatch_32f_C3R_Ctx()
-				
-				// //(for i=0;i<BATCH;++i)
-					//nppiSwapChannels_32f_C3IR_Ctx()             
-				 	 //nppiMulC_32f_C3IRSfs_Ctx()
-						 //nppiSubC_32f_C3RSfs_Ctx/()
-						 //nppiDivC_32f_C3RSfs_Ctx()
-						 //split???? nppiCopy_32f_C3P3R_Ctx
-
+			for (int i = 0; i < BATCH; ++i)
+			{				
+				NPP_CHECK(nppiConvert_8u32f_C3R_Ctx(d_input[i].ptr(), d_input[i].step, d_output[i].ptr(), d_input[i].step(), up, nppcontext));
 			}
-			else if constexpr (CV_MAT_CN(CV_TYPE_I) == 4 && correctDept) {
+			//(for i=0;i<BATCH;++i)
+			// nppiConvert_8u32f_AC3
+			//
+			// nppiResizeBatch_32f_C3R_Ctx()
 
-			}
-			else {
+			// //(for i=0;i<BATCH;++i)
+			// nppiSwapChannels_32f_C3IR_Ctx()
+			// nppiMulC_32f_C3IRSfs_Ctx()
+			// nppiSubC_32f_C3RSfs_Ctx/()
+			// nppiDivC_32f_C3RSfs_Ctx()
+			// split???? nppiCopy_32f_C3P3R_Ctx
+		}
 
-			}
-			//			gpuErrchk(cudaStreamDestroy(stream));
-			
-				// cvGPUSpeedup
-				if constexpr (CV_MAT_CN(CV_TYPE_I) == 3 && correctDept) {
-					cvGS::executeOperations(cv_stream,
-						cvGS::resize<CV_TYPE_I, cv::INTER_LINEAR, BATCH>(crops, up, BATCH),
-						cvGS::cvtColor<cv::COLOR_RGB2BGR, CV_TYPE_O>(),
-						cvGS::multiply<CV_TYPE_O>(val_alpha),
-						cvGS::subtract<CV_TYPE_O>(val_sub),
-						cvGS::divide<CV_TYPE_O>(val_div),
-						cvGS::split<CV_TYPE_O>(d_tensor_output, up));
-				}
-				else if constexpr (CV_MAT_CN(CV_TYPE_I) == 4 && correctDept) {
-					cvGS::executeOperations(cv_stream,
-						cvGS::resize<CV_TYPE_I, cv::INTER_LINEAR, BATCH>(crops, up, BATCH),
-						cvGS::cvtColor<cv::COLOR_RGBA2BGRA, CV_TYPE_O>(),
-						cvGS::multiply<CV_TYPE_O>(val_alpha),
-						cvGS::subtract<CV_TYPE_O>(val_sub),
-						cvGS::divide<CV_TYPE_O>(val_div),
-						cvGS::split<CV_TYPE_O>(d_tensor_output, up));
-				}
-				else {
-					cvGS::executeOperations(cv_stream,
-						cvGS::resize<CV_TYPE_I, cv::INTER_LINEAR, BATCH>(crops, up, BATCH),
-						cvGS::multiply<CV_TYPE_O>(val_alpha),
-						cvGS::subtract<CV_TYPE_O>(val_sub),
-						cvGS::divide<CV_TYPE_O>(val_div),
-						cvGS::split<CV_TYPE_O>(d_tensor_output, up));
-				}
-				d_tensor_output.download(h_tensor_output, cv_stream);
+		// cvGPUSpeedup
+		cvGS::executeOperations(compute_stream,
+								cvGS::resize<CV_TYPE_I, cv::INTER_LINEAR, BATCH>(crops, up, BATCH),
+								cvGS::cvtColor<cv::COLOR_RGB2BGR, CV_TYPE_O>(),
+								cvGS::multiply<CV_TYPE_O>(val_alpha),
+								cvGS::subtract<CV_TYPE_O>(val_sub),
+								cvGS::divide<CV_TYPE_O>(val_div),
+								cvGS::split<CV_TYPE_O>(d_tensor_output, up));
 
-			// Verify results
-			for (int crop_i = 0; crop_i < BATCH; crop_i++) {
-				for (int i = 0; i < CV_MAT_CN(CV_TYPE_O); i++) {
-					d_output_cv[crop_i].at(i).download(h_cvResults[crop_i].at(i), cv_stream);
-				}
-			}
+		d_tensor_output.download(h_tensor_output, compute_stream);
 
-			cv_stream.waitForCompletion();
-
-			for (int crop_i = 0; crop_i < BATCH; crop_i++) {
-				cv::Mat row = h_tensor_output.row(crop_i);
-				for (int i = 0; i < CV_MAT_CN(CV_TYPE_O); i++) {
-					int planeStart = i * up.width * up.height;
-					int planeEnd = ((i + 1) * up.width * up.height) - 1;
-					cv::Mat plane = row.colRange(planeStart, planeEnd);
-					h_cvGSResults[crop_i].push_back(cv::Mat(up.height, up.width, plane.type(), plane.data));
-				}
-			}
-
-			for (int crop_i = 0; crop_i < BATCH; crop_i++) {
-				for (int i = 0; i < CV_MAT_CN(CV_TYPE_O); i++) {
-					cv::Mat cvRes = h_cvResults[crop_i].at(i);
-					cv::Mat cvGSRes = h_cvGSResults[crop_i].at(i);
-					diff = cv::abs(cvRes - cvGSRes);
-					bool passedThisTime = checkResults<CV_MAT_DEPTH(CV_TYPE_O)>(diff.cols, diff.rows, diff);
-					passed &= passedThisTime;
-				}
+		// Verify results
+		for (int crop_i = 0; crop_i < BATCH; crop_i++)
+		{
+			for (int i = 0; i < CV_MAT_CN(CV_TYPE_O); i++)
+			{
+				d_output_cv[crop_i].at(i).download(h_cvResults[crop_i].at(i), compute_stream);
 			}
 		}
-		catch (const cv::Exception& e) {
-			if (e.code != -210) {
-				error_s << e.what();
-				passed = false;
-				exception = true;
+
+		compute_stream.waitForCompletion();
+
+		for (int crop_i = 0; crop_i < BATCH; crop_i++)
+		{
+			cv::Mat row = h_tensor_output.row(crop_i);
+			for (int i = 0; i < CV_MAT_CN(CV_TYPE_O); i++)
+			{
+				int planeStart = i * up.width * up.height;
+				int planeEnd = ((i + 1) * up.width * up.height) - 1;
+				cv::Mat plane = row.colRange(planeStart, planeEnd);
+				h_cvGSResults[crop_i].push_back(cv::Mat(up.height, up.width, plane.type(), plane.data));
 			}
 		}
-		catch (const std::exception& e) {
+
+		for (int crop_i = 0; crop_i < BATCH; crop_i++)
+		{
+			for (int i = 0; i < CV_MAT_CN(CV_TYPE_O); i++)
+			{
+				cv::Mat cvRes = h_cvResults[crop_i].at(i);
+				cv::Mat cvGSRes = h_cvGSResults[crop_i].at(i);
+				diff = cv::abs(cvRes - cvGSRes);
+				bool passedThisTime = checkResults<CV_MAT_DEPTH(CV_TYPE_O)>(diff.cols, diff.rows, diff);
+				passed &= passedThisTime;
+			}
+		}
+	}
+	catch (const cv::Exception &e)
+	{
+		if (e.code != -210)
+		{
 			error_s << e.what();
 			passed = false;
 			exception = true;
 		}
+	}
+	catch (const std::exception &e)
+	{
+		error_s << e.what();
+		passed = false;
+		exception = true;
+	}
 
-		if (!passed) {
-			if (!exception) {
-				std::stringstream ss;
-				ss << "test_npp_batchresize_x_split3D<" << cvTypeToString<CV_TYPE_I>() << ", " << cvTypeToString<CV_TYPE_O>();
-				std::cout << ss.str() << "> failed!! RESULT ERROR: Some results do not match baseline." << std::endl;
-			}
-			else {
-				std::stringstream ss;
-				ss << "test_npp_batchresize_x_split3D<" << cvTypeToString<CV_TYPE_I>() << ", " << cvTypeToString<CV_TYPE_O>();
-				std::cout << ss.str() << "> failed!! EXCEPTION: " << error_s.str() << std::endl;
-			}
+	if (!passed)
+	{
+		if (!exception)
+		{
+			std::stringstream ss;
+			ss << "test_npp_batchresize_x_split3D<" << cvTypeToString<CV_TYPE_I>() << ", " << cvTypeToString<CV_TYPE_O>();
+			std::cout << ss.str() << "> failed!! RESULT ERROR: Some results do not match baseline." << std::endl;
+		}
+		else
+		{
+			std::stringstream ss;
+			ss << "test_npp_batchresize_x_split3D<" << cvTypeToString<CV_TYPE_I>() << ", " << cvTypeToString<CV_TYPE_O>();
+			std::cout << ss.str() << "> failed!! EXCEPTION: " << error_s.str() << std::endl;
 		}
 	}
 
@@ -250,13 +220,15 @@ bool test_npp_batchresize_x_split3D(size_t NUM_ELEMS_X, size_t NUM_ELEMS_Y, cuda
 }
 
 template <size_t... Is>
-bool test_npp_batchresize_x_split3D(const size_t NUM_ELEMS_X, const size_t NUM_ELEMS_Y, std::index_sequence<Is...> seq, cudaStream_t cv_stream, bool enabled) {
+bool test_npp_batchresize_x_split3D(const size_t NUM_ELEMS_X, const size_t NUM_ELEMS_Y, std::index_sequence<Is...> seq, cudaStream_t compute_stream, bool enabled)
+{
 	bool passed = true;
-	int dummy[] = { (passed &= test_npp_batchresize_x_split3D<batchValues[Is]>(NUM_ELEMS_X, NUM_ELEMS_Y, cv_stream, enabled), 0)... };
+	int dummy[] = {(passed &= test_npp_batchresize_x_split3D<batchValues[Is]>(NUM_ELEMS_X, NUM_ELEMS_Y, compute_stream, enabled), 0)...};
 	return passed;
 }
 
-int launch() {
+int launch()
+{
 	constexpr size_t NUM_ELEMS_X = 3840;
 	constexpr size_t NUM_ELEMS_Y = 2160;
 
@@ -267,18 +239,21 @@ int launch() {
 	results["test_npp_batchresize_x_split3D"] = true;
 	std::make_index_sequence<batchValues.size()> iSeq{};
 
-    results["test_npp_batchresize_x_split3D"] &= test_npp_batchresize_x_split3D(NUM_ELEMS_X, NUM_ELEMS_Y, iSeq, stream, true);
+	results["test_npp_batchresize_x_split3D"] &= test_npp_batchresize_x_split3D(NUM_ELEMS_X, NUM_ELEMS_Y, iSeq, stream, true);
 
 	int returnValue = 0;
-	for (const auto& [key, passed] : results) {
-		if (passed) {
+	for (const auto &[key, passed] : results)
+	{
+		if (passed)
+		{
 			std::cout << key << " passed!!" << std::endl;
 		}
-		else {
+		else
+		{
 			std::cout << key << " failed!!" << std::endl;
 			returnValue = -1;
 		}
 	}
-
+	gpuErrchk(cudaStreamDestroy(stream));
 	return returnValue;
 }
