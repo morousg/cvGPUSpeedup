8fa344d3e81639db9328d959ed0b16e455ca1010
morousg <morousg@gmail.com>
2024-08-23 19:30:41 +0200

    Added missing inlcude in 7 benchmark files

--- tests/benchmark/benchmark_vertical_fusionCompareDiv.cu
+++ tests/benchmark/benchmark_vertical_fusionCompareDiv.cu
@@ -12,11 +12,12 @@
    See the License for the specific language governing permissions and
    limitations under the License. */
 
+#include "tests/main.h"
+
 #include "tests/testsCommon.cuh"
+#include <fused_kernel/algorithms/basic_ops/static_loop.cuh>
 #include <cvGPUSpeedup.cuh>
 #include <opencv2/cudaimgproc.hpp>
-
-#include "tests/main.h"
 
 #ifdef ENABLE_BENCHMARK
 constexpr char VARIABLE_DIMENSION[]{ "Number of Operations" };
