a531bddf8ca6091f24c373f8c72fa081ebb2af8b
morousg <morousg@gmail.com>
2024-10-27 19:44:55 +0100

    no message

--- include/fused_kernel/code_snippets/fkl_paper.cuh
+++ include/fused_kernel/code_snippets/fkl_paper.cuh
@@ -1,9 +1,14 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+// This should not go to the paper
 struct UnaryType {};
 struct BinaryType {};
+#define S_ASSERT_INPUT_OUTPUT
+#define STATIC_ASSERT_INSTANCE_TYPE
+// end no to paper
 
+// Figure operations
 template <typename I, typename O>
 struct UnaryOperationName {
   using InputType = I;
@@ -15,11 +20,41 @@ struct UnaryOperationName {
   }
 };
 
-#define S_ASSERT_INPUT_OUTPUT
+template <typename I, typename P, typename O>
+struct BinaryOperationName {
+  using InputType = I;
+  using ParamsType = P;
+  using OutputType = O;
+  using InstanceType = UnaryType;
+  static constexpr __device__ __forceinline__
+  OutputType exec(const InputType &input, const ParamsType &params) {
+    return /*implementation*/;
+  }
+};
+// end Figure operations
 
+// Figure operation instances
+template <typename Op> struct UnaryOperationInstance {
+  using Operation = Op;
+  using InstanceType = UnaryType;
+  // Macro to statically check that Op::InstanceType == UnaryType
+  // Generates a human readable message to better understand the issue
+  STATIC_ASSERT_INSTANCE_TYPE
+};
+
+template <typename Op> struct BinaryOperationInstance {
+  using Operation = Op;
+  using InstanceType = BinaryType;
+  STATIC_ASSERT_INSTANCE_TYPE
+  typename Operation::ParamsType params;
+}; 
+// end Figure operation instances
+
+
+// Figure pointwise
 template <typename R, typename... OpIs>
 constexpr __device__ __forceinline__ auto process_helper(const R &result, const OpIs &...instances) {
-  if constexpr (sizeof…(instances) > 0) {
+  if constexpr (sizeof...(instances) > 0) {
     return process(result, instances…);
   } else {
     return result;
@@ -31,10 +66,10 @@ constexpr __device__ __forceinline__ auto process(const I &inputReg, const First
                                                   const OpIs &...instances) {
   if constexpr (/*FirstOpI::InstanceType  == UnaryType*/) {
     const auto result = FirstOpI::Operation::exec(inputReg);
-    return process_helper(result, instances…);
+    return process_helper(result, instances...);
   } else { // Is BinaryType
     const auto result = FirstOpI::Operation::exec(inputReg, instance.params);
-    return process_helper(result, instances…);
+    return process_helper(result, instances...);
   }
 }
 
@@ -42,5 +77,6 @@ template <typename I, typename O, typename... OpIs>
 __global__ void pointwise(const I input, O output, const OpIs... instances) {
   S_ASSERT_INPUT_OUTPUT     // Check that I/O types match
       const I inputReg = 0; // read input for current thread
-  output[index] = process(inputReg, instances…);
+  output[index] = process(inputReg, instances...);
 }
+// end Figure pointwise
