43c74fbe3ed42e06c90dbd18da07d252a0738fd8
Albert Andaluz <albertandaluz@hotmail.com>
2025-05-19 18:10:09 +0200

    renamed  mysampleverticalfusionapp to benchmark_vertical_fusionCompareMul1

--- benchmarks/opencv/verticalfusion/vertical_fusion_kernel_instances/mul1/launcher.cu
+++ benchmarks/opencv/verticalfusion/vertical_fusion_kernel_instances/mul1/launcher.cu
@@ -21,8 +21,8 @@
 
  
 // --- End Dummies ---
-int main() {
-  /* // Dummy data for the function call
+int launch() {
+   // Dummy data for the function call
   std::array<cv::cuda::GpuMat, REAL_BATCH> crops_data;
   cv::cuda::Stream stream_data;
   float alpha_data = 1.0f;
@@ -32,11 +32,11 @@ int main() {
   
  
  std::cout << "Launching experiment for N=1 via Mul Pipeline..." << std::endl;
- launchMulPipeline<1>(crops_data, stream_data, alpha_data, output_tensor_data, crop_size_data, func_data);
+ launchMulPipeline<5>(crops_data, stream_data, alpha_data, output_tensor_data, crop_size_data, func_data);
   
   
   // ... and so on, or use a loop if you pass N at runtime (which launchMulPipeline is not designed for)
   cudaDeviceSynchronize(); // Wait for all CUDA work to finish
-  std::cout << "All launched experiments synchronized." << std::endl;*/
+  std::cout << "All launched experiments synchronized." << std::endl;
   return 0;
 }
